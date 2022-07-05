d1bf3ed0586f5cf19374fdf5948f01970d6688ed
Herby <herbyosbourne31@gmail.com>
2022-07-05 15:50:25 -0500

    adding compression libary for gzipping on heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());

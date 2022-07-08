1b28eadbe028bb59d6a70e69c43f8791321ca4ef
Herby <herbyosbourne31@gmail.com>
2022-07-08 09:14:35 -0500

    added getuserRef to createCart or getUserCart by id from firestore

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -54,6 +54,19 @@ export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
   return await batch.commit();
 };
 
+export const getUserCartRef = async (userId) => {
+  const cartRef = firestore.collection('carts').where('userId', '==', userId);
+  const snapShot = await cartRef.get();
+
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection('carts').doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
+};
+
 export const convertCollectionSnapshotToMap = (collections) => {
   const transformedCollection = collections.docs.map((doc) => {
     const { title, items } = doc.data();

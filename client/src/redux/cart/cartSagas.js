aa2d88e329cc22fb58af0df2aad68d9112fa2557
Herby <herbyosbourne31@gmail.com>
2022-07-08 09:21:01 -0500

    update cart saga to handle onCartChange to update firestore when ADD_ITEM,REDUCE_ITEM,EMPTY_CART, onUserSignIn function check if user has cartItems and load it

--- client/src/redux/cart/cartSagas.js
+++ client/src/redux/cart/cartSagas.js
@@ -1,6 +1,10 @@
-import { takeLatest, put, call, all } from 'redux-saga/effects';
+import { takeLatest, put, call, all, select } from 'redux-saga/effects';
+import cartActionTypes from './cartTypes';
 import userActionTypes from '../user/userTypes';
-import { emptyCart } from './cartActions';
+import { getUserCartRef } from '../../firebase/firebase';
+import { emptyCart, setCartFromFirebase } from './cartActions';
+import { selectCurrentUser } from '../user/userSelectors';
+import { selectCartItems } from '../cart/cartSelector';
 
 export function* emptyCartOnSignOut() {
   yield put(emptyCart());
@@ -10,6 +14,38 @@ export function* onSignOutSuccess() {
   yield takeLatest(userActionTypes.SIGN_OUT_SUCCESS, emptyCartOnSignOut);
 }
 
+export function* checkCartFromFirebase(payload) {
+  const { user } = payload;
+  // yield console.log(user);
+  const cartRef = yield getUserCartRef(user.id);
+  const cartSnapshot = yield cartRef.get();
+  yield put(setCartFromFirebase(cartSnapshot.data().cartItems));
+}
+
+export function* onUserSignIn() {
+  yield takeLatest(userActionTypes.SIGN_IN_SUCCESS, checkCartFromFirebase);
+}
+
+export function* updateCartInFirebase() {
+  const currentUser = yield select(selectCurrentUser);
+  if (currentUser) {
+    try {
+      const cartRef = yield getUserCartRef(currentUser.id);
+      const cartItems = yield select(selectCartItems);
+      yield cartRef.update({ cartItems });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export function* onCartChange() {
+  yield takeLatest(
+    [cartActionTypes.ADD_ITEM, cartActionTypes.REDUCE_ITEM, cartActionTypes.EMPTY_CART],
+    updateCartInFirebase
+  );
+}
+
 export function* cartSaga() {
-  yield all([call(onSignOutSuccess)]);
+  yield all([call(onSignOutSuccess), call(onUserSignIn), call(onCartChange)]);
 }
